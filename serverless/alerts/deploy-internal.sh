5e6b36abf662f02ff88fcbe66a84beb91e8c610f
jpremshanker <prem.shanker@creditsaison-in.com>
2024-01-17 14:01:12 +0530

    PCPE-830 | Refactor Alerts

--- serverless/alerts/deploy-internal.sh
+++ serverless/alerts/deploy-internal.sh
@@ -8,6 +8,28 @@ AWS_PROFILE=$4
 
 echo "Deploying $STACK_NAME core template on $1"
 
+build_lambda() {
+  (
+    echo "-----Packaging $1 lambda-----"
+    cd "$1" || exit
+    sam build
+    sam package \
+      --output-template-file package.yaml \
+      --s3-bucket "${BUCKET_NAME}" \
+      --region "$AWS_REGION" \
+      --profile $AWS_PROFILE
+  )
+}
+
+
+for dir in */; do
+      if [ -d "$dir" ]; then
+         build_lambda "$dir" &
+      fi
+done
+
+wait
+
 sam package --template-file master.yaml \
 --output-template-file temp-template.template \
 --s3-bucket "$BUCKET_NAME" \
@@ -21,6 +43,10 @@ sam deploy --stack-name $STACK_NAME --template-file temp-template.template \
 --profile "$AWS_PROFILE" \
 --parameter-overrides EnvironmentType="$ENV"
 
+# Clean Up
+rm -rf */package.yaml
+rm -rf */.aws-sam
 rm -f temp-template.template
+rm -rf .env
 
 echo "$STACK_NAME template deployed successfully"
